84cc50ebb250fba2ac34f9b037531b701bad5253
Sandro Rodrigues <srtabs@gmail.com>
2017-05-22 00:32:10 +0100

    Hard changes on models and migrations reset

--- alexandria_docs/projects/models.py
+++ alexandria_docs/projects/models.py
@@ -1,7 +1,6 @@
 from __future__ import unicode_literals
 
 import tarfile
-import hashlib
 import os
 
 from django.utils.translation import ugettext_lazy as _
@@ -15,7 +14,8 @@ from django_extensions.db.models import (
     TitleSlugDescriptionModel, TimeStampedModel)
 from taggit.managers import TaggableManager
 
-from projects.validators import MimeTypeValidator
+from projects.validators import MimeTypeValidator, IntegrityTarValidator
+from projects.managers import ImportedFileManager
 from projects.utils import projects_upload_to
 
 
@@ -53,6 +53,10 @@ class Project(TitleSlugDescriptionModel, TimeStampedModel):
     def get_absolute_url(self):
         return "{}{}/index.html".format(settings.PROJECTS_SERVE_URL, self.slug)
 
+    @property
+    def serve_root_path(self):
+        return os.path.join(settings.PROJECTS_SERVE_ROOT, self.slug)
+
 
 @python_2_unicode_compatible
 class ImportedArchive(TimeStampedModel):
@@ -65,10 +69,11 @@ class ImportedArchive(TimeStampedModel):
         Project, models.CASCADE, verbose_name=_('project'))
     archive = models.FileField(
         _('archive'), upload_to=projects_upload_to,
-        help_text=_('archive with project documentation'),
+        help_text=_('archive with project documentation.'),
         validators=[
             MimeTypeValidator(
-                allowed_mimetypes=settings.PROJECTS_ALLOWED_MIMETYPES)
+                allowed_mimetypes=settings.PROJECTS_ALLOWED_MIMETYPES),
+            IntegrityTarValidator(settings.PROJECTS_SERVE_ROOT)
         ])
 
     class Meta:
@@ -77,28 +82,18 @@ class ImportedArchive(TimeStampedModel):
     def __str__(self):
         return self.project.__str__()
 
-    @property
-    def extract_path(self):
-        return os.path.join(settings.PROJECTS_SERVE_ROOT, self.project.slug)
+    def fileify(self):
+        """Extract tarfile and fileify valid files"""
+        with tarfile.open(self.archive.path, "r:gz") as tar:
+            tar.extractall(self.project.serve_root_path)
+        ImportedFile.objects.walk(self.project_id,
+                                  self.project.serve_root_path)
 
     @staticmethod
-    def post_save(sender, instance, **kwargs):
+    def post_save(sender, instance, created, **kwargs):
         """Extract the archive and put files to be served"""
-        with tarfile.open(instance.archive.path, "r:gz") as tar:
-            tar.extractall(instance.extract_path)
-
-        for root, __, filenames in os.walk(instance.extract_path):
-            for filename in filenames:
-                full_path = os.path.join(root, filename)
-                md5 = hashlib.md5(open(full_path, 'rb').read()).hexdigest()
-                obj, __ = ImportedFile.objects.update_or_create(
-                    imported_archive=instance,
-                    name=filename,
-                    path=full_path,
-                    defaults={
-                        'md5': md5
-                    }
-                )
+        if created:
+            instance.fileify()
 
 
 post_save.connect(ImportedArchive.post_save, sender=ImportedArchive)
@@ -106,17 +101,19 @@ post_save.connect(ImportedArchive.post_save, sender=ImportedArchive)
 
 @python_2_unicode_compatible
 class ImportedFile(TimeStampedModel):
-    """ """
-    imported_archive = models.ForeignKey(
-        settings.AUTH_USER_MODEL, models.CASCADE, verbose_name=_('archive'))
-    name = models.CharField(_('Name'), max_length=255)
-    path = models.FilePathField(
-        path=settings.PROJECTS_SERVE_ROOT, match=".*\.html$", recursive=True,
-        max_length=255)
+    """Holds info about html files uploaded for indexing proposes."""
+    project = models.ForeignKey(
+        Project, models.CASCADE, verbose_name=_('project'))
+    path = models.CharField(_('file path'), max_length=255)
     md5 = models.CharField(_('MD5 checksum'), max_length=255)
+    objects = ImportedFileManager()
 
     class Meta:
         verbose_name = _('imported file')
 
     def __str__(self):
-        return self.name
+        return self.path
+
+    def get_absolute_url(self):
+        relpath = os.path.relpath(self.path, settings.PROJECTS_SERVE_ROOT)
+        return settings.PROJECTS_SERVE_URL + relpath
