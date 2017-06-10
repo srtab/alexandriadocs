6ace34f5abde1f36938286f70457f38956c94776
Sandro Rodrigues <srtabs@gmail.com>
2017-06-10 02:06:52 +0100

    Added html extractor.

---
+++ alexandria_docs/search/extractors.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.utils.html import strip_tags
+
+from lxml.html.clean import Cleaner
+from pyquery import PyQuery
+
+
+class HtmlExtractor(object):
+    """
+    Extract relevant content from html document.
+
+    ``kill_tags``:
+        A list of tags to kill. Killing also removes the tag's content,
+        i.e. the whole subtree, not just the tag itself.
+
+    ``content_tags``:
+        A list of tags to search the most relevant content from the document.
+        Order of tags is important, only the first match will be used.
+
+    ``title_tags``:
+        A list of tags to search title of document. Order of tags is important,
+        only the first match will be used.
+    """
+
+    kill_tags = ['footer']
+    content_tags = ['[role=main]', 'body']
+    title_tags = ['h1', 'h2', 'head title']
+
+    def __init__(self, html):
+        self.doc = PyQuery(html)
+
+    @property
+    def content(self):
+        """ """
+        for path in self.content_tags:
+            try:
+                content = self.doc(path).html()
+                if not content:
+                    continue
+                return self.strip_all(content)
+            except ValueError:
+                continue
+        return None
+
+    @property
+    def title(self):
+        """ """
+        for path in self.title_tags:
+            try:
+                title = self.doc(path).eq(0).html()
+                if not title:
+                    continue
+                return self.strip_all(title)
+            except ValueError:
+                continue
+        return None
+
+    def strip_all(self, html):
+        """Clean html content striping all html tags and removing all inline
+        script and style."""
+        cleaner = Cleaner(style=True, kill_tags=self.kill_tags)
+        cleaned_html = cleaner.clean_html(html)
+        text = strip_tags(cleaned_html).replace('¶', '')
+        return " ".join(text.split())
---
+++ alexandria_docs/search/tests/test_extractor.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from mock import Mock
+
+from django.test import SimpleTestCase
+
+from search.extractors import HtmlExtractor
+from pyquery import PyQuery
+
+
+class HtmlExtractorText(SimpleTestCase):
+
+    def test_init(self):
+        extractor = HtmlExtractor("<div></div>")
+        self.assertIsNotNone(extractor.doc)
+        self.assertIsInstance(extractor.doc, PyQuery)
+
+    def test_content(self):
+        doc = "<body>Dummy Content</body>"
+        extractor = HtmlExtractor(doc)
+        self.assertEqual(extractor.content, "Dummy Content")
+
+    def test_content_no_body(self):
+        doc = "<div>Dummy Content</div>"
+        extractor = HtmlExtractor(doc)
+        self.assertIsNone(extractor.content)
+
+    def test_content_raise_value_error(self):
+        extractor = HtmlExtractor("<div></div>")
+        extractor.doc = Mock(side_effect=ValueError())
+        self.assertIsNone(extractor.content)
+
+    def test_title(self):
+        doc = "<h1>Dummy Title</h1>"
+        extractor = HtmlExtractor(doc)
+        self.assertEqual(extractor.title, "Dummy Title")
+
+    def test_content_no_title(self):
+        doc = "<div>Dummy Title</div>"
+        extractor = HtmlExtractor(doc)
+        self.assertIsNone(extractor.title)
+
+    def test_title_raise_value_error(self):
+        extractor = HtmlExtractor("<h1></h1>")
+        extractor.doc = Mock(side_effect=ValueError())
+        self.assertIsNone(extractor.title)
