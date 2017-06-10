b01d75ae285ba4cef4d4116892aa0233a6f906ba
Sandro Rodrigues <srtabs@gmail.com>
2017-06-10 03:10:15 +0100

    Fixed search counts and improved unit tests

--- alexandria_docs/search/tests/test_views.py
+++ alexandria_docs/search/tests/test_views.py
@@ -5,7 +5,7 @@ from django.urls import reverse
 from django.test import TestCase
 
 
-class SearchViewTest(TestCase):
+class SearchProjectViewTest(TestCase):
 
     def setUp(self):
         self.url = reverse('search:index')
@@ -14,3 +14,22 @@ class SearchViewTest(TestCase):
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed("search/index.html")
+
+    def test_search(self):
+        response = self.client.get(self.url, {'q': 'serach'})
+        self.assertEqual(response.status_code, 200)
+
+
+class SearchPageViewTest(TestCase):
+
+    def setUp(self):
+        self.url = reverse('search:pages')
+
+    def test_template_used(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed("search/index.html")
+
+    def test_search(self):
+        response = self.client.get(self.url, {'q': 'serach'})
+        self.assertEqual(response.status_code, 200)
