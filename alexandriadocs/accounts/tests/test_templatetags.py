e00927638ebe37217cc6b11292d1bfe9f75b8ca2
Sandro Rodrigues <srtabs@gmail.com>
2017-08-27 17:09:40 +0100

    Added unit tests for accounts templatetags

---
+++ alexandriadocs/accounts/tests/test_templatetags.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from accounts.templatetags.accounts_tags import get_providers_unconnected
+from django.test import SimpleTestCase
+from mock import Mock, patch
+
+
+class GetProvidersUnconnectedTest(SimpleTestCase):
+    """ """
+
+    def setUp(self):
+        self.providers_available = [Mock(id='prov1'), Mock(id='prov2')]
+
+    @patch('accounts.templatetags.accounts_tags.providers')
+    @patch('accounts.templatetags.accounts_tags.SocialAccount')
+    def test_providers_unconnected(self, msocial, mproviders):
+        msocial.objects.filter().values_list.return_value = ['prov1']
+        mproviders.registry.get_list.return_value = self.providers_available
+        result = get_providers_unconnected('user')
+        self.assertEqual(result, [self.providers_available[1]])
+
+    @patch('accounts.templatetags.accounts_tags.providers')
+    @patch('accounts.templatetags.accounts_tags.SocialAccount')
+    def test_no_providers_unconnected(self, msocial, mproviders):
+        msocial.objects.filter().values_list.return_value = ['prov1', 'prov2']
+        mproviders.registry.get_list.return_value = self.providers_available
+        result = get_providers_unconnected('user')
+        self.assertEqual(result, [])
