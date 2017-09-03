7ad7e9e3d67d933ab20006eb6aedd7e593ca7aca
Sandro Rodrigues <srtabs@gmail.com>
2017-09-03 22:40:37 +0100

    Added redis to docker-compose

--- alexandriadocs/alexandriadocs/settings/docker.py
+++ alexandriadocs/alexandriadocs/settings/docker.py
@@ -29,8 +29,12 @@ HAYSTACK_CONNECTIONS = {
 
 
 CACHES = {
-    'default': {
-        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://redis:6379/0",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
     }
 }
 
