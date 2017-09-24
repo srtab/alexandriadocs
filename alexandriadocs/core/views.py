774310d8954bee4f41ff8b46ec1c0dcb43fa9183
Sandro Rodrigues <srtabs@gmail.com>
2017-09-24 22:45:52 +0100

    Added autocomplete to groups and user selections

--- alexandriadocs/core/views.py
+++ alexandriadocs/core/views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
+from django.http import JsonResponse
 from django.views.generic import ListView
-
+from django.views.generic.list import BaseListView
 from projects.models import Project
 
 
@@ -12,3 +13,27 @@ class HomepageView(ListView):
 
     def get_queryset(self):
         return self.model._default_manager.public()
+
+
+class BaseSelect2View(BaseListView):
+    """ """
+    paginate_by = 10
+
+    def dispatch(self, request, *args, **kwargs):
+        self.term = request.GET.get('term', None)
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_options(self, context):
+        return [{
+            'id': str(result.pk),
+            'text': str(result),
+        } for result in context['object_list']]
+
+    def render_to_response(self, context):
+        """Return a JSON response in Select2 format."""
+        return JsonResponse({
+            'results': self.get_options(context),
+            'pagination': {
+                'more': context['page_obj'].has_next()
+            }
+        })
--- alexandriadocs/static/js/main.jquery.js
+++ alexandriadocs/static/js/main.jquery.js
@@ -7,7 +7,28 @@
     $('[data-toggle="tooltip"]').tooltip();
     $('[data-dj-messages-noty]').djMessagesNoty();
     $('[data-ajax-submit]').djangoAjaxForms();
-    $('select').select2();
+
+    $('.select2').each(function() {
+      var $this = $(this),
+          options = {};
+
+      if ($this.data('autocomplete-url')) {
+        options.ajax = {
+          url: $this.data('autocomplete-url'),
+          delay: 250,
+          cache: true,
+          data: function (params) {
+            var query = {
+              term: params.term,
+              page: params.page || 1
+            }
+            return query;
+          },
+        }
+      }
+
+      $(this).select2(options);
+    });
   });
 
 })(jQuery);
