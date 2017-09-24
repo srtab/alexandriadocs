0597c1844846118279101125ccca586575a8d717
Sandro Rodrigues <srtabs@gmail.com>
2017-09-24 18:06:47 +0100

    Integrated select2

--- alexandriadocs/static/js/main.jquery.js
+++ alexandriadocs/static/js/main.jquery.js
@@ -6,7 +6,8 @@
   $(document).ready(function() {
     $('[data-toggle="tooltip"]').tooltip();
     $('[data-dj-messages-noty]').djMessagesNoty();
-    $('[data-ajax-submit]').djangoAjaxForms()
+    $('[data-ajax-submit]').djangoAjaxForms();
+    $('select').select2();
   });
 
 })(jQuery);
