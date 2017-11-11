880d8428d181b2e55cb70646af1de95e0643bcf3
Sandro Rodrigues <sfr@eurotux.com>
2017-11-11 18:18:06 +0000

    Removed subdomain support

--- alexandriadocs/alexandriadocs/settings/__init__.py
+++ alexandriadocs/alexandriadocs/settings/__init__.py
@@ -67,7 +67,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'projects.middleware.SubdomainMiddleware'
 ]
 
 TEMPLATES = [
