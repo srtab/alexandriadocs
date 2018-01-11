bbe3ea16644bf3e97af2458cb454e53079608610
Sandro Rodrigues <srtabs@gmail.com>
2018-01-11 23:25:03 +0000

    Updated frontend dependencies

--- alexandriadocs/static/vendor/noty/index.d.ts
+++ alexandriadocs/static/vendor/noty/index.d.ts
@@ -1,132 +1,121 @@
-	declare module "noty" {
-		namespace Noty {
-			type Type = 'alert' | 'success' | 'warning' | 'error' | 'info' | 'information';
-			type Theme = 'mint' | 'sunset' | 'relax' | 'metroui' | 'bootstrap-v3' | 'bootstrap-v4' | 'semanticui' | 'nest';
-			type Layout = 'top' | 'topLeft' | 'topCenter' | 'topRight' | 'center' | 'centerLeft' | 'centerRight' | 'bottom' | 'bottomLeft' | 'bottomCenter' | 'bottomRight';
-			type Event = 'beforeShow' | 'onShow' | 'afterShow' | 'onClose' | 'afterClose' | 'onHover' | 'onTemplate';
-	
+declare module 'noty' {
+	export = Noty;
+}
 
-			interface Button {
-				new(text: string, classNames: string, cb: Function, attributes: any) : Noty.Button
-			}
-	
+declare class Noty {
+	constructor(options?: Noty.Options);
 
-			interface Options {
-				type?: Noty.Type;
-				layout?: Noty.Layout;
-				theme?: Noty.Theme;
-				text?: string;
-				timeout?: false | number;
-				progressBar?: boolean;
-				closeWith?: ('click' | 'button')[];
-				animation?: {
-					open?: string | null | Function,
-					close?: string | null | Function
-				};
-				id?: false | string;
-				force?: boolean;
-				killer?: boolean | string;
-				queue?: string;
-				container?: false | string;
-				buttons?: Noty.Button[],
-				callbacks?: {
-					beforeShow?: () => void,
-					onShow?: () => void,
-					afterShow?: () => void,
-					onClose?: () => void,
-					afterClose?: () => void,
-					onHover?: () => void,
-					onTemplate?: () => void
-				};
-				sounds?: {
-					sources?: string[],
-					volume?: number,
-					conditions?: string[]
-				};
-				docTitle?: {
-					conditions?: string[]
-				};
-				modal?: boolean,
-			}
-		}
-	
+	/**
+	 * Show a NOTY
+	 */
+	show: () => void;
 
-		class Noty {
-			constructor(options?: Noty.Options);
-	
+	/**
+	 * Close a NOTY
+	 */
+	close: () => void;
 
-			/**
-			 * Show a NOTY
-			 */
-			show: () => void;
-	
+	/**
+	 * Notification text updater. Important: .noty_body class is required for setText API method
+	 */
+	setText: (text: string, overrideConstructorOption?: true) => void;
 
-			/**
-			 * Close a NOTY
-			 */
-			close: () => void;
-	
+	/**
+	 * Notification type updater
+	 */
+	setType: (type: Noty.Type, overrideConstructorOption?: true) => void;
 
-			/**
-			 * Notification text updater. Important: .noty_body class is required for setText API method.
-			 */
-			setText: (text: string, overrideConstructorOption?: true) => void;
-	
+	/**
+	 * Notification theme updater
+	 */
+	setTheme: (theme: Noty.Theme, overrideConstructorOption?: true) => void;
 
-			/**
-			 * Notification type updater
-			 */
-			setType: (type: Noty.Type, overrideConstructorOption?: true) => void;
-	
+	/**
+	 * false (clears timeout) or integer (clears timer, starts for given value)
+	 */
+	setTimeout: (option: false | number) => void; //
 
-			/**
-			 * Notification theme updater
-			 */
-			setTheme: (theme: Noty.Theme, overrideConstructorOption?: true) => void;
-	
+	/**
+	 * Clears the timeout
+	 */
+	stop: () => void;
 
-			/**
-			 * false (clears timeout) or integer (clears timer, starts for given value)
-			 */
-			setTimeout: (option: false | number) => void; //
-	
+	/**
+	 * Restarts the timeout
+	 */
+	resume: () => void;
 
-			/**
-			 * Clears the timeout
-			 */
-			stop: () => void;
-	
+	/**
+	 * Register event handlers for Noty outside of constructior options
+	 * Important: You need to call on() methods before the show() method
+	 */
+	on: (eventName: Noty.Event, callback: Function) => void;
 
-			/**
-			 * Restarts the timeout
-			 */
-			resume: () => void;
-	
+	/**
+	 * Without queue name: Closes all notifications
+	 * With queue name: Closes all notifications for the named queue
+	 */
+	static closeAll: (queueName?: string) => void;
 
-			/**
-			 * Register event handlers for Noty outside of constructior options.
-			 * Important: You need to call on() methods before the show() method.
-			 */
-			on: (eventName: Noty.Event, callback: Function) => void;
-	
+	/**
+	 * Without queue name: Sets the maxVisible notification count for global queue
+	 * With parameter: Sets the maxVisible notification count for the named queue
+	 */
+	static setMaxVisible: (max: number, queueName?: string) => void;
 
-			/**
-			 * Without queue name: Closes all notifications.
-			 * With queue name: Closes all notifications for the named queue
-			 */
-			static closeAll: (queueName?: string) => void;
-	
+	/**
+	 * Change default values for new instances of NOTY
+	 */
+	static overrideDefaults: (obj: { [i: string]: any }) => Noty;
 
-			/**
-			 * Without queue name: Sets the maxVisible notification count for global queue.
-			 * With parameter: Sets the maxVisible notification count for the named queue;
-			 */
-			static setMaxVisible: (max: number, queueName?: string) => void;
-	
+	static button: (text: string, classNames: string, cb: Function, attributes?: any) => Noty.Button;
+}
 
-			static button: (text: string, classNames: string, cb: Function, attributes?: any) => Noty.Button;
-		}
-	
+declare namespace Noty {
+	type Type = 'alert' | 'success' | 'warning' | 'error' | 'info' | 'information';
+	type Theme = 'mint' | 'sunset' | 'relax' | 'metroui' | 'bootstrap-v3' | 'bootstrap-v4' | 'semanticui' | 'nest';
+	type Layout = 'top' | 'topLeft' | 'topCenter' | 'topRight' | 'center' | 'centerLeft' | 'centerRight' | 'bottom' | 'bottomLeft' | 'bottomCenter' | 'bottomRight';
+	type Event = 'beforeShow' | 'onShow' | 'afterShow' | 'onClose' | 'afterClose' | 'onHover' | 'onTemplate';
 
-		export = Noty;
+	interface Button {
+		new(text: string, classNames: string, cb: Function, attributes: any) : Noty.Button
 	}
+
+	interface Options {
+		type?: Noty.Type;
+		layout?: Noty.Layout;
+		theme?: Noty.Theme;
+		text?: string;
+		timeout?: false | number;
+		progressBar?: boolean;
+		closeWith?: ('click' | 'button')[];
+		animation?: {
+			open?: string | null | Function,
+			close?: string | null | Function
+		};
+		id?: false | string;
+		force?: boolean;
+		killer?: boolean | string;
+		queue?: string;
+		container?: false | string;
+		buttons?: Noty.Button[],
+		callbacks?: {
+			beforeShow?: () => void,
+			onShow?: () => void,
+			afterShow?: () => void,
+			onClose?: () => void,
+			afterClose?: () => void,
+			onHover?: () => void,
+			onTemplate?: () => void
+		};
+		sounds?: {
+			sources?: string[],
+			volume?: number,
+			conditions?: string[]
+		};
+		docTitle?: {
+			conditions?: string[]
+		};
+		modal?: boolean,
+	}
+}
--- alexandriadocs/static/vendor/popper.js/packages/test/karma.conf.js
+++ alexandriadocs/static/vendor/popper.js/packages/test/karma.conf.js
@@ -3,15 +3,18 @@ const path = require('path');
 const babel = require('rollup-plugin-babel');
 const resolve = require('rollup-plugin-node-resolve');
 
-const browsers = (argv.browsers || process.env.BROWSERS || 'ChromeHeadless').split(',');
+const browsers = (argv.browsers ||
+  process.env.BROWSERS ||
+  'ChromeHeadless'
+).split(',');
 const singleRun = process.env.NODE_ENV === 'development' ? false : true;
-const coverage = process.env.COVERAGE === true;
+const coverage = process.env.COVERAGE === 'true';
 const basePath = process.cwd();
 
 const babelrc = {
   babelrc: false,
   presets: [
-    [require.resolve('babel-preset-es2015'), { modules: false }],
+    [require.resolve('babel-preset-env'), { modules: false }],
     require.resolve('babel-preset-stage-2'),
   ],
   plugins: [
@@ -94,11 +97,16 @@ module.exports = function(config) {
         version: '11',
         platform: 'Windows 10',
       },
-      SLiOS9: {
+      // Currently not used because the iOS emulator isn't reliable and
+      // most of the times it times out
+      SLiOS11: {
         base: 'SauceLabs',
-        browserName: 'iphone',
-        version: '9.3',
-        platform: 'macOS 10.12',
+        browserName: 'Safari',
+        deviceName: 'iPhone 8 Plus Simulator',
+        deviceOrientation: 'portrait',
+        platformVersion: '11.0',
+        platformName: 'iOS',
+        appiumVersion: '1.7.1',
       },
       SLChromeMobile: {
         base: 'SauceLabs',
@@ -117,7 +125,7 @@ module.exports = function(config) {
 
     rollupPreprocessor: {
       format: 'umd',
-      sourceMap: 'inline',
+      sourcemap: 'inline',
       globals: {
         chai: 'chai',
         'popper-utils': 'PopperUtils',
@@ -136,15 +144,31 @@ module.exports = function(config) {
       recordVideo: true,
       tunnelIdentifier: process.env.TRAVIS_JOB_NUMBER,
     },
-    coverageReporter: {
+    reporters: ['mocha', 'saucelabs'],
+    plugins: [
+      require('karma-chai'),
+      require('karma-chrome-launcher'),
+      require('karma-coverage'),
+      require('karma-firefox-launcher'),
+      require('karma-jasmine'),
+      require('karma-mocha-reporter'),
+      require('karma-rollup-preprocessor'),
+      require('karma-safari-launcher'),
+      require('karma-sauce-launcher'),
+      require('karma-sinon'),
+    ],
+  };
+
+  if (coverage) {
+    configuration.coverageReporter = {
       dir: './.tmp/coverage',
       reporters: [
         { type: 'html', subdir: 'report-html' },
         { type: 'lcov', subdir: 'report-lcov' },
       ],
-    },
-    reporters: ['mocha', 'saucelabs', 'coverage'],
-  };
+    };
+    configuration.reporters.push('coverage');
+  }
 
   config.set(configuration);
 };
